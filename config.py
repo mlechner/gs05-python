b733f1e4859f4273a644c8eed79ef43b66838ef6
Marco Lechner, GeoBoink <lechner@geoboink.de>
2018-11-23 19:52:55 +0100

    initial commit

---
+++ config.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import time
+import serial
+
+ser = serial.Serial(
+        "/dev/ttyAMA0",
+        baudrate=9600,
+        bytesize=serial.EIGHTBITS,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+        timeout=1
+        )
+
+counter=0
+while 1:
+    x=ser.readline()
+    print x
