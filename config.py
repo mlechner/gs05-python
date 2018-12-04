80bec50a443e461f1471f839a3df20fb8f5670b1
Marco Lechner, GeoBoink <marco@marcolechner.de>
2018-12-04 22:07:57 +0100

    modified config.py for being a simple test

--- config.py
+++ config.py
@@ -4,15 +4,15 @@ import time
 import serial
 
 ser = serial.Serial(
-        "/dev/ttyAMA0",
-        baudrate=9600,
-        bytesize=serial.EIGHTBITS,
-        parity=serial.PARITY_NONE,
-        stopbits=serial.STOPBITS_ONE,
-        timeout=1
-        )
+    "/dev/ttyAMA0",
+    baudrate=9600,
+    bytesize=serial.EIGHTBITS,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    timeout=1
+)
 
-counter=0
 while 1:
-    x=ser.readline()
-    print x
+    ser.write(b'D')
+    print(ser.read(256))
+    time.sleep(10)
